eac9f8b832cf560a6c2a85d2fd3eecb44a2c76f6
Ismail <ismail.smd1981@gmail.com>
2022-08-06 17:42:26 +0400

    inputs

--- 03-inputs.sh
+++ 03-inputs.sh
@@ -3,4 +3,6 @@
 read -p "Enter your name: " name
 read -p "Course name: " course
 
-echo $name $course
+echo Your name -  $name, Course name - $course
+
+#During execution of automation this kind of inputs won't work.Hence this is not prefarable
